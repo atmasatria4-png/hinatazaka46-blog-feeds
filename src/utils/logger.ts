e3c02afaf5c870d6bbaa5b472c83f7a7df9dbfd8
alfianchii <pianaee379@gmail.com>
2025-09-10 14:59:13 +0700

    feat: check logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,12 @@
 import type { Logger } from "../types/log";
 
+const currentTime = new Date().toISOString()
+
 export const log: Logger = {
-  base: (msg: string): void => console.log(`[INFO ${new Date().toISOString()}] ${msg}`),
-  info: (msg: string): void => console.log(`[INFO ${new Date().toISOString()}] ✨ ${msg}`),
-  success: (msg: string): void => console.log(`[INFO ${new Date().toISOString()}] ✅ ${msg}`),
-  warn: (msg: string): void => console.log(`[INFO ${new Date().toISOString()}] ⚠️ ${msg}`),
-  error: (msg: string): void => console.log(`[INFO ${new Date().toISOString()}] ❌ ${msg}`),
+  base: (msg: string): void => console.log(`[INFO ${currentTime}] ${msg}`),
+  info: (msg: string): void => console.log(`[INFO ${currentTime}] ✨ ${msg}`),
+  success: (msg: string): void => console.log(`[INFO ${currentTime}] ✅ ${msg}`),
+  check: (msg: string): void => console.log(`[INFO ${currentTime}] 🔍 ${msg}`),
+  warn: (msg: string): void => console.log(`[INFO ${currentTime}] ⚠️ ${msg}`),
+  error: (msg: string): void => console.log(`[INFO ${currentTime}] ❌ ${msg}`),
 }
